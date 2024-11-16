6e6003abc7ba60386df899482e4b7bf99508c6e4
Prathik N K <135017249+prathiknk8055@users.noreply.github.com>
2024-11-17 00:27:06 +0530

    Update fulladder.sv

--- Day 3 of RTL-Coding/fulladder.sv
+++ Day 3 of RTL-Coding/fulladder.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.11.2024 23:48:18
-// Design Name: 
-// Module Name: fulladder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module fulladder(
     input a,
