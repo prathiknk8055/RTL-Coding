a7ba299ba843d6d769504008f1a7a8883f62a3c2
Prathik N K <135017249+prathiknk8055@users.noreply.github.com>
2024-11-22 00:09:54 +0530

    Update fullsubtractor_tb.sv

--- Day 3 of RTL-Coding/fullsubtractor_tb.sv
+++ Day 3 of RTL-Coding/fullsubtractor_tb.sv
@@ -1,11 +1,8 @@
 `timescale 1ns / 1ps
 
 module tb_fullsubtractor;
-
-    // Inputs
+    
     reg a, b, cin;
-
-    // Outputs
     wire diff, bout;
 
     // Instantiate the Unit Under Test (UUT)
@@ -19,20 +16,20 @@ module tb_fullsubtractor;
 
     // Test procedure
     initial begin
-        integer i; // Loop variable
+        integer i; 
 
-        // Display header for results
+       
         $display("Time | a b cin | diff bout");
         $display("----------------------------");
 
-        // Loop through all possible input combinations (3 inputs = 2^3 = 8 combinations)
+        
         for (i = 0; i < 8; i = i + 1) begin
-            {a, b, cin} = i; // Assign values based on loop index
-            #10; // Wait for 10 time units
+            {a, b, cin} = i; 
+            #10; 
             $display("%4t | %b %b  %b   |  %b    %b", $time, a, b, cin, diff, bout);
         end
 
-        // End simulation
+     
         $finish;
     end
 endmodule
